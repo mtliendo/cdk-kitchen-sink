61bb8a735431e52fb3b1f7a6d23db2ee9d5fbf76
Michael Liendo <mtliendo@amazon.com>
2022-11-16 11:45:27 -0600

    update with base, but has cyclic dep error

---
+++ bin/cdk-kitchen-sink.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { AuthStack } from '../lib/AuthStack'
+import { FileStorageStack } from '../lib/fileStorageStack'
+import { DatabaseStack } from '../lib/DatabaseStack'
+import { APIStack } from '../lib/APIStack'
+
+const app = new cdk.App()
+
+const authStack = new AuthStack(app, 'AuthStackSamples', {})
+
+const apiStack = new APIStack(app, 'AppSyncAPIStackSamples', {
+	userpool: authStack.userpool,
+	sampleTable: databaseStack.sampleTable,
+	unauthenticatedRole: authStack.unauthenticatedRole,
+})
+
+const databaseStack = new DatabaseStack(app, 'DatabaseStackSamples', {})
+const fileStorageStack = new FileStorageStack(app, 'FileStorageStackSamples', {
+	authenticatedRole: authStack.authenticatedRole,
+	unauthenticatedRole: authStack.unauthenticatedRole,
+	allowedOrigins: ['http://localhost:3000'],
+})
